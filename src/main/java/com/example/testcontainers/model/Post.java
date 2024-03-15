0398d8a3cec1e15efe2312c4827f030f1ff3a1cd
Stanislaav Kozh <printks@yandex.ru>
2024-03-15 22:31:54 +0300

    Initial commit

---
+++ src/main/java/com/example/testcontainers/model/Post.java
@@ -0,0 +1,32 @@
+package com.example.testcontainers.model;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Table(name = "posts")
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 1024)
+    private String title;
+
+    @Column(length = 16384)
+    private String text;
+
+    private long viewsAmount;
+
+    public Post(String title, String text) {
+        this.title = title;
+        this.text = text;
+    }
+
+}
---
+++ src/main/java/com/example/testcontainers/web/mapper/Mappable.java
@@ -0,0 +1,13 @@
+package com.example.testcontainers.web.mapper;
+
+import java.util.List;
+
+public interface Mappable<E, D> {
+
+    E toEntity(D d);
+
+    D toDto(E e);
+
+    List<D> toDto(List<E> e);
+
+}
